c66e1a78779a9ab00a6c335ab3bd0e46860c27c7
Owen Blacker <owen.blacker@madetech.com>
2023-12-01 15:19:26 +0000

    Unarchive before add collaborators when necessary; also:
    * use strict
    * enumerate repos that were unarchived or errored

--- batch-maintainer.js
+++ batch-maintainer.js
@@ -1,14 +1,17 @@
+"use strict";
 import {Octokit} from "octokit";
 import 'dotenv/config.js';
 
-let errorCount = 0;
-let successCount = 0;
+let errorList = [];
+let successList = [];
+let archiveList = [];
 const reposList = JSON.parse(process.env.reposList)
 const pat = process.env.pat;
 const verbose = process.env.verbose || "undefined";
 const userToEscalate = process.env.userToEscalate;
 const repoOwner = 'alphagov';
 const escalationPrivilege = 'maintain';
+const apiVersion = '2022-11-28';
 const octokit = new Octokit({
     auth: `${pat}`
 })
@@ -16,35 +19,66 @@ const octokit = new Octokit({
 console.log(`=== Escalating ${userToEscalate} in a list of ${reposList.length} repos with PAT ending ${pat.slice(-4)}...`);
 
 for (let i = 0; i < reposList.length; i++) {
+    let wasRepoArchived = false;
     try {
+        const repo = await octokit.request('GET /repos/{owner}/{repo}', {
+            owner: repoOwner,
+            repo: reposList[i],
+            headers: { 'X-GitHub-Api-Version': apiVersion }
+        })
+        if (repo.data.archived === true)
+        {
+            wasRepoArchived = true;
+            archiveList.push(reposList[i]);
+
+            const unarchive = await octokit.request('PATCH /repos/{owner}/{repo}', {
+                owner: repoOwner,
+                repo: reposList[i],
+                archived: 'false',
+                headers: { 'X-GitHub-Api-Version': apiVersion }
+            })
+
+            if (response.status !== 200)
+            {
+                console.error(` 🟡 \x1bError unarchiving \x1b[33m${repoOwner}/${reposList[i]}\x1b[0m. Message: “${response.data.message}”`)
+            } else if (verbose)
+            {
+                console.log(`Unarchived \x1b[33m${repoOwner}/${reposList[i]}\x1b[0m`)
+            }
+        }
         const response = await octokit.request('PUT /repos/{owner}/{repo}/collaborators/{username}', {
             owner: repoOwner,
             repo: reposList[i],
             username: userToEscalate,
             permission: escalationPrivilege,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
+            headers: { 'X-GitHub-Api-Version': apiVersion }
         })
         if(response.status === 204){
-            console.log(` ✅ ${userToEscalate} has been made a maintainer of \x1b[33m${repoOwner}/${reposList[i]}\x1b[0m`)
-            successCount++
+            console.log(` ✅ ${userToEscalate} has been made a maintainer of \x1b[33m${repoOwner}/${reposList[i]}\x1b[0m`);
+            successList.push(reposList[i]);
         }else if (response.status === 201)
         {
             console.log(` ✅ ${userToEscalate} has been invited as a maintainer of \x1b[33m${repoOwner}/${reposList[i]}\x1b[0m`)
-            successCount++
+            successList.push(reposList[i]);
         }
     } catch (error) {
         if (error.response) {
             console.error(` ❌ \x1b[31mError with escalating ${userToEscalate} for \x1b[1;4m${repoOwner}/${reposList[i]}\x1b[0;31m!\x1b[0m Status: ${error.response.status}. Message: “${error.response.data.message}”`)
-            errorCount++;
+            errorList.push(reposList[i]);
         }
     }
     if (verbose !== "false")
     {
-        console.log("=== Repos amended so far: ", successCount);
-        console.log("=== Number of errors so far: ", errorCount);
+        console.log("=== Repos amended so far: ", successList.length, (archiveList.length > 0) ? `, of which ${archiveList.length} had been archived.` : '');
+        console.log("=== Number of errors so far: ", errorList.length);
     }
 }
-console.log(`=== ${successCount} repos have been successfully amended`)
-console.log(`=== There were ${errorCount} errors`)
+console.log(`=== ${successList.length} repos have been successfully amended`, (archiveList.length > 0) ? `, of which ${archiveList.length} had been archived.` : '')
+
+if (archiveList.length > 0)
+{
+     console.log(`===>> The following repos will need re-archiving: ['${archiveList.join("','")}']. You probably want to copy/paste this list to somewhere.`);
+}
+
+console.log(`=== There were ${errorList.length} errors`,
+    (errorList.length > 0) ? `: ['${errorList.join("','")}'].` : '.')
